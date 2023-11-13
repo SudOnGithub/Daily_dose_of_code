f5c7b1837054e7f982f36445980d42d0f1b88be0
SudOnGithub <142712282+SudOnGithub@users.noreply.github.com>
2023-11-13 16:30:28 +0530

    Zerodha landing page

---
+++ HTML + CSS/Zerodha Clone/app.js
@@ -0,0 +1,15 @@
+function calc(){
+
+let result;
+
+let princ = document.getElementById('p').value ;
+
+let rat = document.getElementById('r').value ;
+
+let tim = document.getElementById('t').value ;
+
+result = (princ*rat*tim)/100
+
+} 
+console.log(result)
+
