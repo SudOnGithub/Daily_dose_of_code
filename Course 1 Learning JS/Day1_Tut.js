018bc8a158bd69dcee119f01d90468e05bec41db
SudOnGithub <142712282+SudOnGithub@users.noreply.github.com>
2023-11-19 20:05:56 +0530

    JS1

---
+++ Course 1 Learning JS/Day1_Tut.js
@@ -0,0 +1,22 @@
+// Change value of var anywhere
+
+var gamelevel="1";
+gamelevel="2";
+gamelevel="3";
+
+alert("Your level is currently: " + gamelevel);
+
+// Alert your message using concatenation
+
+var myName="Sudhir";
+var yourName= prompt("What is your Name?")
+
+alert("Hi " + yourName + ", Welcome to " + myName + "'s Webpage!");
+
+// Subtract length of 2 strings
+var a="Sudhir";
+var b="Nakul";
+
+alert(a.length - b.length);
+
+
