8d3511d79cd0b09cf753c0c8947108d8c6c44bea
SudOnGithub <142712282+SudOnGithub@users.noreply.github.com>
2023-11-24 20:14:43 +0530

    JS1

---
+++ Course 1 Learning JS/Love_calc.js
@@ -0,0 +1,8 @@
+var firstName = prompt("Enter name here: ");
+var secondName = prompt("Enter name here: ");
+var score = Math.random();
+score = (score * 100) + 1;
+score = Math.floor(score);
+
+
+alert("Your love score is: " + score);
