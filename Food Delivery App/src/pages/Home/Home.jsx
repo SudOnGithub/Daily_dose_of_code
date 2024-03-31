5ec74b3040f5302128695b9fd99102b80d35c440
SudOnGithub <142712282+SudOnGithub@users.noreply.github.com>
2024-04-01 01:00:05 +0530

    Food delivery App day2

--- Food Delivery App/src/pages/Home/Home.jsx
+++ Food Delivery App/src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './Home.css'
+import React, {useState} from "react";
+import "./Home.css";
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [category, setCategory] = useState("All");
 
-export default Home
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  );
+};
+
+export default Home;
